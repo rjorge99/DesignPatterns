43115b0b203dbf9c631b0a5429b1d5682dbab0eb
Jorge Reyes <rjorge99@hotmail.com>
2022-07-08 15:12:59 -0500

    Singleton

---
+++ singleton.js
@@ -0,0 +1,34 @@
+// Sample 1
+const Singleton = (function () {
+    let instance;
+
+    function constructor() {
+        return {
+            method() {}
+        };
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) instance = constructor();
+            return instance;
+        }
+    };
+})();
+
+const s1 = Singleton.getInstance();
+const s2 = Singleton.getInstance();
+console.log(s1 === s2);
+
+// Sample 2
+const INSTANCE = Symbol('instance');
+function Singleton2() {
+    if (Singleton2[INSTANCE]) return Singleton2[INSTANCE];
+    return (Singleton2[INSTANCE] = {
+        method() {}
+    });
+}
+
+const s11 = Singleton2();
+const s22 = Singleton2();
+console.log(s11 === s22);
