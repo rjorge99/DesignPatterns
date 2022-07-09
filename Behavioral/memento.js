c0e044d271c91a7c62257afbe79332ec4b29855f
Jorge Reyes <rjorge99@hotmail.com>
2022-07-09 09:08:51 -0500

    Memento added

---
+++ Behavioral/memento.js
@@ -0,0 +1,67 @@
+// The Memento pattern provides temporary storage as well as restoration of an object.
+// Optional, employee.createMemento() could return directly the clone, and not a memento object
+
+class Memento {
+    constructor(employee) {
+        this.employee = employee;
+    }
+}
+
+class CareTaker {
+    constructor() {
+        this.mementos = [];
+        this.current = -1;
+    }
+
+    add(memento) {
+        this.mementos.push(memento);
+        this.current++;
+    }
+
+    getCurrent() {
+        return this.mementos[this.current];
+    }
+
+    next() {
+        this.current++;
+        return this.mementos[this.current];
+    }
+
+    previous() {
+        this.current--;
+        return this.mementos[this.current];
+    }
+}
+
+class Employee {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    createMemento() {
+        return new Memento({ ...this });
+    }
+
+    restoreMemento(memento) {
+        let employee = memento.employee;
+        this.name = employee.name;
+        this.age = employee.age;
+    }
+}
+
+let employee = new Employee('John', 30);
+let caretaker = new CareTaker();
+
+caretaker.add(employee.createMemento());
+
+employee.name = 'Jane';
+employee.age = 40;
+caretaker.add(employee.createMemento());
+
+employee.name = 'Jack';
+employee.age = 50;
+caretaker.add(employee.createMemento());
+
+employee.restoreMemento(caretaker.previous()); // Jane
+employee.restoreMemento(caretaker.previous()); // John
