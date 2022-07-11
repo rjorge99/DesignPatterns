34d7a82d3529b4e1cb577e93b449e001e77e3e32
Jorge Reyes <rjorge99@hotmail.com>
2022-07-11 11:28:05 -0500

    Sample 2 added to flyweight

--- Structural/flyweight.js
+++ Structural/flyweight.js
@@ -48,3 +48,80 @@ playList.addPlayItem('Song 1');
 playList.addPlayItem('Song 2');
 playList.addPlayItem('Song 3');
 playList.addPlayItem('Song 2'); // <-- Donst create a new object
+
+// In our example code we are building computers
+// Many models, makes, and processor combinations are common, so these characteristics are factored out and shared by Flyweight objects.
+// The FlyweightFactory maintains a pool of Flyweight objects.
+// Sample 2
+
+// Common properties for the computers
+class FlyWeight {
+    constructor(make, model, processor) {
+        this.make = make;
+        this.model = model;
+        this.processor = processor;
+    }
+
+    make() {}
+}
+
+// Fabrica de Flyweights, en caso de que exista, regresa la ya creada, en caso contrario crea una nueva
+class FlyWeightFactory {
+    static flyweights = {};
+
+    static get(make, model, processor) {
+        if (this.flyweights[make + model]) return this.flyweights[make + model];
+
+        this.flyweights[make + model] = new FlyWeight(make, model, processor);
+    }
+
+    static getCount() {
+        let count = 0;
+        for (let key in this.flyweights) count++;
+        return count;
+    }
+}
+
+class Computer {
+    constructor(make, model, processor, memory, tag) {
+        this.flyWeight = FlyWeightFactory.get(make, model, processor);
+        this.memory = memory;
+        this.tag = tag;
+    }
+
+    getMake() {
+        return this.flyWeight.make;
+    }
+}
+
+class ComputerCollection {
+    constructor() {
+        this.computers = {};
+        this.count = 0;
+    }
+
+    add(make, model, processor, memory, tag) {
+        this.computers[tag] = new Computer(make, model, processor, memory, tag);
+        this.count++;
+    }
+
+    get(tag) {
+        return this.computers[tag];
+    }
+
+    getCount() {
+        return this.count;
+    }
+}
+
+const computers = new ComputerCollection();
+computers.add('Dell', 'Studio XPS', 'Intel', '5G', 'Y755P');
+computers.add('Dell', 'Studio XPS', 'Intel', '6G', 'X997T');
+computers.add('Dell', 'Studio XPS', 'Intel', '2G', 'U8U80');
+computers.add('Dell', 'Studio XPS', 'Intel', '2G', 'NT777');
+computers.add('Dell', 'Studio XPS', 'Intel', '2G', '0J88A');
+computers.add('HP', 'Envy', 'Intel', '4G', 'CNU883701');
+computers.add('HP', 'Envy', 'Intel', '2G', 'TXU003283');
+
+console.log(`Computers in collection: ${computers.getCount()}`); // 7
+console.log(`Flyweights in factory: ${FlyWeightFactory.getCount()}`); // 2
