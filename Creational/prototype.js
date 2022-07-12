c60a39cb75d259c80eae55f85aa0e170556672cb
Jorge Reyes <rjorge99@hotmail.com>
2022-07-12 13:25:46 -0500

    Prototype

---
+++ Creational/prototype.js
@@ -0,0 +1,41 @@
+// The Prototype Pattern creates new objects, but rather than creating non-initialized objects
+// it returns objects that are initialized with values it copied from a prototype
+// The Prototype pattern is also referred to as the Properties pattern.
+// The prototype object holds the default values that are copied over into a newly created business object.
+// Creates objects based on a template of an existing object through cloning.
+
+// Sample 1
+let myCar = {
+    name: 'Ford',
+    drive: function () {},
+    panic: function () {}
+};
+
+let yourCar = Object.create(myCar);
+// Now we can see that one is a prototype of the other
+console.log(yourCar.name); // Ford
+
+// Sample 2
+// If we wish to implement the Prototype pattern without directly using Object.create,
+// we can simulate the pattern as per the above example
+let vehiclePrototype = {
+    init: function (carModel) {
+        this.model = carModel;
+    },
+
+    getModel: function () {
+        return this.model;
+    }
+};
+
+function vehicle(model) {
+    function F() {}
+
+    F.prototype = vehiclePrototype;
+    let f = new F();
+    f.init(model);
+    return f;
+}
+
+let car = vehicle('Ford');
+car.getModel(); // Ford
