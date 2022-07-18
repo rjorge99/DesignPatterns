06e9b3affac0bbab6982a653f368199cad77d02e
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 15:16:34 -0500

    Command

---
+++ Behavioral/command.js
@@ -0,0 +1,3 @@
+// The Command pattern encapsulates actions as objects.
+// we can decouple objects that execute a certain task from the object that calls the method.
+// its duty is to split the communication to separate objects so that they become loosely coupled while still maintaining the end goal.
