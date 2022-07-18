b6313c2cbd50276a42518d168d42b7cc5d961c3b
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 14:44:02 -0500

    Sample ` added to chain of responsibility

--- Behavioral/chainofresponsibility.js
+++ Behavioral/chainofresponsibility.js
@@ -6,3 +6,80 @@
 //      Coffee Maker
 //      ATM Machine
 //      Transformers: Utilize a chain of transformer/parsers in some custom AST
+
+// Sample 1
+const handleColors = (obj, key, value, next) => {
+    if (typeof value === 'string' && value.startsWith('0x'))
+        obj[key] = value.replace('0x', '#');
+
+    next();
+};
+
+const handleSizes = (obj, key, value, next) => {
+    if (/(width|height|fontSize)/.test(key)) {
+        const isDec = Number(value) % 1 !== 0;
+        obj[key] = String(value).endsWith('px')
+            ? value
+            : `${String(isDec ? value * 100 : value)}px`;
+    }
+    next();
+};
+
+class TNode {
+    next = null;
+
+    constructor(callback) {
+        this.callback = callback;
+    }
+
+    execute(...args) {
+        return this.callback(...args, () => {
+            this.next && this.next.execute(...args);
+        });
+    }
+}
+
+function createTransform(...transformers) {
+    transformers = transformers.slice().reverse();
+
+    function withNode(transformer) {
+        return new TNode(transformer);
+    }
+
+    let executor = withNode(transformers.pop());
+    let curr = executor;
+
+    while (transformers.length) {
+        curr.next = withNode(transformers.pop());
+        curr = curr.next;
+    }
+
+    function transform(obj) {
+        for (const [key, value] of Object.entries(obj)) {
+            executor.execute(obj, key, value);
+        }
+
+        return obj;
+    }
+
+    return transform;
+}
+
+const rowData = {
+    backgroundColor: '0xFF0000',
+    fontSize: '12',
+    width: '0.9',
+    height: '0.5',
+    color: '0xFFFFFF'
+};
+
+const transform = createTransform(handleColors, handleSizes);
+const result = transform(rowData);
+console.log(result);
+// {
+//   "backgroundColor": "#020303",
+//   "color": "#201313",
+//   "fontSize": "14px",
+//   "width": "90px",
+//   "height": "50px",
+// }
