19548e67d9a71f4f1fe71ab50c3b6902ba09d3d9
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 14:18:52 -0500

    Sample 3 added to facade

--- Structural/facade.js
+++ Structural/facade.js
@@ -2,6 +2,7 @@
 // his pattern provides a convenient higher-level interface to a larger body of code, hiding its true underlying complexity
 // Think of it as simplifying the API being presented to other developers
 // The facade pattern aims to provide a simplified way to interact with multiple components by creating a single API.
+// It is also a common approach to use a Facade to interact with a third-party library or some complex native functionalities
 
 // Sample 1
 
@@ -102,3 +103,115 @@ music.get(1).then(console.log).catch(console.error);
 
 const video = new CultureFacade(TYPE_VIDEO);
 video.get(3).then(console.log).catch(console.error);
+
+// Sample 2
+class API {
+    constructor(authtoken) {
+        this.authtoken = authtoken;
+    }
+
+    constructHeaders() {
+        const headers = new Headers();
+        headers.set('Authorization', this.authtoken);
+        return headers;
+    }
+
+    handleResponse(response) {
+        if (response.ok) return response.json();
+        return promise.reject({ status: response.status, message: response.statusText });
+    }
+
+    get(url, options) {
+        return fetch(url, {
+            headers: this.constructHeaders(),
+            ...options
+        }).then(this.handleResponse);
+    }
+
+    post(url, options) {
+        return fetch(url, {
+            method: 'POST',
+            headers: this.constructHeaders(),
+            ...options
+        }).then(this.handleResponse);
+    }
+
+    put(url, options) {
+        return fetch(url, {
+            method: 'PUT',
+            headers: this.constructHeaders(),
+            ...options
+        }).then(this.handleResponse);
+    }
+
+    delete(url, options) {
+        return fetch(url, {
+            method: 'DELETE',
+            headers: this.constructHeaders(),
+            ...options
+        }).then(this.handleResponse);
+    }
+}
+
+const api = new API('Bearer 12345');
+api.get('https://jsonplaceholder.typicode.com/users/1').then(console.log).catch(console.error);
+
+// Sample 3 Applied to React (Use of custom hooks to group useStates for example)
+// https://wanago.io/2019/12/09/javascript-design-patterns-facade-react-hooks/
+
+// custom hook
+function useUsersManagement() {
+    const [users, setUsers] = useState([]);
+
+    function addUser(user) {
+        setUsers([...users, user]);
+    }
+
+    function deleteUser(userId) {
+        setUsers(users.filter((user) => user.id !== userId));
+    }
+
+    return {
+        users,
+        addUser,
+        deleteUser
+    };
+}
+
+// custom hook
+function useAddUserModalManagement() {
+    conts[(isAddUserModalOpen, setIsAddUserModalOpen)] = useState(false);
+
+    function openAddUserModal() {
+        setIsAddUserModalOpen(true);
+    }
+
+    function closeAddUserModal() {
+        setIsAddUserModalOpen(false);
+    }
+
+    return {
+        isAddUserModalOpen,
+        openAddUserModal,
+        closeAddUserModal
+    };
+}
+
+// Component
+function Users() {
+    const { users, addUser, deleteUser } = useUsersManagement();
+    const { isAddUserModalOpen, openAddUserModal, closeAddUserModal } =
+        useAddUserModalManagement();
+
+    return (
+        <div>
+            <button onClick={openAddUserModal}>Add user</button>
+            <UsersList users={users} deleteUser={deleteUser} />
+            <AddUserModal
+                isOpen={isAddUserModalOpen}
+                close={closeAddUserModal}
+                addUser={addUser}
+            />
+        </div>
+    );
+}
