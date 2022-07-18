7d2093c5524bae3f46aa17e551ee6746575c7876
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 17:23:54 -0500

    Sample added to visitor

--- Behavioral/visitor.js
+++ Behavioral/visitor.js
@@ -53,3 +53,127 @@ target.accept(enlarge);
 
 console.log(target.getSpeed());
 console.log(target.getSize());
+
+// Sample 2
+function Employee(name, salary) {
+    this.name = name;
+    this.salary = salary;
+}
+
+Employee.prototype = {
+    getSalary: function () {
+        return this.salary;
+    },
+    setSalary: function (sal) {
+        this.salary = sal;
+    },
+    accept: function (visitorFunction) {
+        visitorFunction(this);
+    }
+};
+
+const devsage = new Employee('DevSage', 10000);
+console.log(devsage.getSalary()); // 10000
+
+function ExtraSalary(emp) {
+    emp.setSalary(emp.getSalary() * 2);
+}
+
+devsage.accept(ExtraSalary);
+console.log(devsage.getSalary()); // 20000
+
+// Sample 3
+function Node(val) {
+    this.value = val;
+    this.left = this.right = null;
+}
+
+Node.prototype.accept = function (visitorObj) {
+    visitorObj.visit(this);
+    if (this.left) this.left.accept(visitorObj);
+    if (this.right) this.right.accept(visitorObj);
+};
+
+function visitor() {
+    var that = this;
+    this.visit = function (tgt) {
+        tgt.value = '*' + tgt.value;
+    };
+    this.highlight = function (tgt) {
+        tgt.accept(that);
+    };
+}
+
+var tree = new node('A');
+tree.left = new node('B1');
+tree.right = new node('B2');
+tree.left.left = new node('C1');
+tree.left.right = new node('C2');
+new visitor().highlight(tree.left);
+
+// Sample 4
+var Employee = function (name, salary, vacation) {
+    var self = this;
+
+    this.accept = function (visitor) {
+        visitor.visit(self);
+    };
+
+    this.getName = function () {
+        return name;
+    };
+
+    this.getSalary = function () {
+        return salary;
+    };
+
+    this.setSalary = function (sal) {
+        salary = sal;
+    };
+
+    this.getVacation = function () {
+        return vacation;
+    };
+
+    this.setVacation = function (vac) {
+        vacation = vac;
+    };
+};
+
+var ExtraSalary = function () {
+    this.visit = function (emp) {
+        emp.setSalary(emp.getSalary() * 1.1);
+    };
+};
+
+var ExtraVacation = function () {
+    this.visit = function (emp) {
+        emp.setVacation(emp.getVacation() + 2);
+    };
+};
+
+function run() {
+    var employees = [
+        new Employee('John', 10000, 10),
+        new Employee('Mary', 20000, 21),
+        new Employee('Boss', 250000, 51)
+    ];
+
+    var visitorSalary = new ExtraSalary();
+    var visitorVacation = new ExtraVacation();
+
+    for (var i = 0, len = employees.length; i < len; i++) {
+        var emp = employees[i];
+
+        emp.accept(visitorSalary);
+        emp.accept(visitorVacation);
+        console.log(
+            emp.getName() +
+                ': $' +
+                emp.getSalary() +
+                ' and ' +
+                emp.getVacation() +
+                ' vacation days'
+        );
+    }
+}
