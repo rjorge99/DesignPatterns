e55e61cafd5486964ffae0a3ed8c033375de155b
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 15:34:27 -0500

    Se corrige ejemplo de command

--- Behavioral/command.js
+++ Behavioral/command.js
@@ -4,6 +4,27 @@
 
 // Sample 1
 // https://www.patterns.dev/posts/command-pattern
+// Original class before pattern
+class OrderManagerOriginal {
+    constructor() {
+        this.orders = [];
+    }
+
+    placeOrder(order, id) {
+        this.orders.push(id);
+        return `You have successfully ordered ${order} (${id})`;
+    }
+
+    trackOrder(id) {
+        return `Your order ${id} will arrive in 20 minutes.`;
+    }
+
+    cancelOrder(id) {
+        this.orders = this.orders.filter((order) => order.id !== id);
+        return `You have canceled your order ${id}`;
+    }
+}
+
 class OrderManager {
     constructor() {
         this.orders = [];
@@ -23,22 +44,23 @@ class Command {
 function PlaceOrderCommand(order, id) {
     return new Command((orders) => {
         orders.push(id);
-        return `You have successfully ordered ${order} (${id})`;
+        console.log(`You have successfully ordered ${order} (${id})`);
     });
 }
 
 function CancelOrderCommand(id) {
     return new Command((orders) => {
         orders = orders.filter((order) => order.id !== id);
-        return `You have canceled your order ${id}`;
+        console.log(`You have canceled your order ${id}`);
     });
 }
 
 function TrackOrderCommand(id) {
-    return new Command(() => `Your order ${id} will arrive in 20 minutes.`);
+    return new Command(() => console.log(`Your order ${id} will arrive in 20 minutes.`));
 }
 
 const manager = new OrderManager();
+
 manager.execute(new PlaceOrderCommand('Pad Thai', '1234'));
 manager.execute(new TrackOrderCommand('1234'));
 manager.execute(new CancelOrderCommand('1234'));
