c64d7fdfdcd95ebd3b7c477a240ece63a75125a0
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 16:03:24 -0500

    Mediator

---
+++ Behavioral/mediator.js
@@ -0,0 +1,2 @@
+// The Mediator pattern provides central authority over a group of objects by encapsulating how these objects interact
+// This model is useful for scenarios where there is a need to manage complex conditions in which every object is aware of any state change in any other object in the group.
