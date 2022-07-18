9c6050fa745b4c3831bcee0a07e97a798c393912
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 15:53:43 -0500

    Interpreter

---
+++ Behavioral/interpreter.js
@@ -0,0 +1,3 @@
+// The Interpreter pattern offers a scripting language that allows end-users to customize their solution
+// You could offer a basic scripting language which allows the end-user to manipulate your application through simple instructions.
+// The Interpreter pattern helps to convert information from one language into another
