6758204c80e0685bfdd779ece727b0d2ca0d195b
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 12:57:23 -0500

    Composite

---
+++ Structural/composite.js
@@ -0,0 +1,3 @@
+// The Composite pattern allows the creation of objects with properties that are primitive items or a collection of objects
+// Each item in the collection can hold other collections themselves, creating deeply nested structures.
+// A tree control is a perfect example of a Composite pattern
