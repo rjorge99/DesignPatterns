049fceb71f4e553fdc17dff9b7cc3623885012ef
Jorge Reyes <rjorge99@hotmail.com>
2022-07-18 13:44:28 -0500

    Facade

---
+++ Structural/facade.js
@@ -0,0 +1,4 @@
+// provides an interface which shields clients from complex functionality in one or more subsystems
+// his pattern provides a convenient higher-level interface to a larger body of code, hiding its true underlying complexity
+// Think of it as simplifying the API being presented to other developers
+// The facade pattern aims to provide a simplified way to interact with multiple components by creating a single API.
